0913a79a386ecb4a63ac016f7bbf67f8f7ad9c62
Juanchi36 <juanmartingon@hotmail.com>
2019-11-01 16:48:17 -0300

    Agregado de get event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,10 +2,32 @@
 
 var mongoose = require('mongoose');
 var Event = require('../models/Event');
+var isodate = require('isodate');
 //var vareventController = require('./eventControllerService');
 
 module.exports.getEvent = function getEvent(req, res, next) {
-  vareventController.getEvent(req.swagger.params, res, next);
+  //vareventController.getEvent(req.swagger.params, res, next);
+  mongoose.connect('mongodb://127.0.0.1:27017/testDb', {}).then(
+    () => {
+      
+      var idMin = isodate(req.query.fecha_desde);
+      var idMax = isodate(req.query.fecha_hasta);
+
+      if(!req.query.egmid) {
+        Event.find({fecha:{$gt: idMin, $lt: idMax}}, function(err, events) {
+          if (err) throw err;
+          res.send(200, events);
+        });
+      }else{
+        Event.find({ $and: [ { fecha: {$gt: idMin, $lt: idMax}}, { egmid: req.query.egmid}]}, function(err, events) {
+          if (err) throw err;
+          res.send(200, events);
+        });
+      }
+        
+    },
+    err => { /** handle initial connection error */ }
+  );
 };
 
 module.exports.addEvent = function addEvent(req, res, next) {
